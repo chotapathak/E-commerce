a2ee67b64a4eceaa742e13b773f1cec87bd091ef
vikashpathak <chotapathak12@gmail.com>
2022-10-18 18:36:53 +0530

    enabled to public

--- showcom/pages/index.js
+++ showcom/pages/index.js
@@ -1,7 +1,6 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-// import MyApp from './_app'
 import React from 'react'
 import {HeroBanner, Product , FooterBanner} from '../components'
 import { client } from '../lib/client'
