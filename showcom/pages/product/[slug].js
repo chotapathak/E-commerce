9061b5826187667ba9bb6b48fee32eaa03a7ec94
vikashpathak <chotapathak12@gmail.com>
2022-10-21 16:08:25 +0530

    product section ready

--- showcom/pages/product/[slug].js
+++ showcom/pages/product/[slug].js
@@ -1,39 +1,99 @@
-import React, {useState} from "react";
-import {client, urlFor} from '../../lib/client'
-import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
-import { Product } from '../../components';
+import React, { useState } from "react";
+import { client, urlFor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+import { Product } from "../../components";
 
-const ProductDetails = ({product , products}) => {
+const ProductDetails = ({ product, products }) => {
   // console.log(product)
-  const {image, name, details, price} = product;
+  const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
 
   return (
     <div>
-    <div className="product-detail-container">
-      <div className="Imge-container">
-        <img src={urlFor(image && image[index])} className='product-detail-image' />
+      <div className="product-detail-container">
+        <div className="Imge-container">
+          <img
+            src={urlFor(image && image[index])}
+            className="product-detail-image"
+          />
+        </div>
+        <div className="small-images-container">
+          {image?.map((item, i) => (
+            <img
+              key={1}
+              src={urlFor(item)}
+              className={
+                i == index ? "small-image selected-image" : "small-image"
+              }
+              onMouseEnter={() => setIndex(i)}
+            />
+          ))}
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus" onclick="">
+                {" "}
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onclick="">
+                0
+              </span>
+              <span className="plus" onclick="">
+                {" "}
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onclick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onclick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
       </div>
-      <div className='small-images-container'>
-      {image?.map((item, i) => (
-        <img
-        key={1}
-        src={urlFor(item)}
-        className={i == index ? 'small-image selected-image' : 'small-image'}
-        onMouseEnter={() => setIndex(i)}
-        />
-      ))}
-    </div>
-    </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-
-// function to get static path
+// function async to get static path
 export const getStaticPaths = async () => {
-
-    const query = `*[_type == "product"] {
+  const query = `*[_type == "product"] {
     slug {
       current
     }
@@ -44,33 +104,27 @@ export const getStaticPaths = async () => {
   // mapping product path to product slug
   const paths = products.map((product) => ({
     params: {
-      slug: product.slug.current
-    }
+      slug: product.slug.current,
+    },
   }));
   // returns path and fallback function
   return {
     paths,
-    fallback: 'blocking'
-  }
-}
-
+    fallback: "blocking",
+  };
+};
 
 // function ot generate page at StaticSide
-export const getStaticProps = async ({params: { slug } }) => {
-
+export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product"  && slug.current == '${slug}'][0]`;
-  const productsQuery =  '*[_type == "product"]';
+  const productsQuery = '*[_type == "product"]';
 
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
-
   return {
-    props: {products, product}
-  }
-}
-
-
-
+    props: { products, product },
+  };
+};
 
 export default ProductDetails;
